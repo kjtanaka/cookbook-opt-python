b09362baff6e3811573e2af2f327f8169f985476
Koji Tanaka <kj.tanaka@gmail.com>
2014-11-14 14:00:21 -0500

    update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -5,5 +5,6 @@ license          'Apache 2.0'
 description      'Installs/Configures python as an optional software package'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.1'
+supports         'centos', "= 6.5"
 
 depends 'build-essential'
